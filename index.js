a379cd32ff69e7135207babb783d5682e64a09f8
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2023-01-13 10:12:42 -0500

    dfiycgsis

--- index.js
+++
@@ -1,9 +0,0 @@
-function greet(name) {
-  const greetPromise = new Promise(function (resolve, reject) {
-    resolve(`hello ${name}`);
-  });
-  return greetPromise;
-}
-
-const suzie = greet("suzie");
-console.log(suzie);
